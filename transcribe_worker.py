32cad440fb367ca52fedf92331a39186ee504f78
MOAB1184 <moab1184@example.com>
2025-06-04 23:38:21 -0700

    h

--- transcribe_worker.py
+++ transcribe_worker.py
@@ -1,6 +1,5 @@
 import os
 from rq import Worker, Queue
-from rq.connection import Connection
 import redis
 from transcription_tasks import process_transcription_job
 
@@ -13,6 +12,5 @@ redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(list(map(str, listen)))
-        worker.work() 
+    worker = Worker(list(map(str, listen)), connection=conn)
+    worker.work() 
